93fa179f093bdcf011500e940cd761d867c2a503
Hector Lopez <daniel130300@outlook.com>
2020-12-01 03:09:31 -0600

    Ultimos Cambios

--- frontend-ds/frontend-desa/src/app/components/dashboard/dashboard.component.ts
+++ frontend-ds/frontend-desa/src/app/components/dashboard/dashboard.component.ts
@@ -9,19 +9,144 @@ import { AppService } from 'src/app/app.service';
   templateUrl: './dashboard.component.html',
   styleUrls: ['../menu/pandora.component.css', '../menu/containerIndex.component.css'],
 })
-export class GetDashboardComponent implements OnInit
+
+export class GetDashboardComponent
 {
 
+    private test: number[]  = [];
+    private nombre_top = [];
+    private top = [];
+    private ventas_label = [];
+    private ventas_normales_data = []; 
+    private ventas_plus_data = []; 
     public ventas_con_devolucion: number = 0;
     public ventas_sin_devolucion: number = 0;
-    public listado_ventas_normales_anuales: any[] = [];
-    public listado_ventas_plus_anuales: any[] = [];
-    public listado_top_5_productos: any[] = [];
-    public listado_ventas_no_devoluciones: any[] = [];
-    public listado_ventas_si_devoluciones: any[] = [];
+    public listado_ventas_normales_anuales: any[];
+    public listado_ventas_plus_anuales: any[];
+    public listado_top_5_productos: any[];
+    public listado_ventas_no_devoluciones: any[];
+    public listado_ventas_si_devoluciones: any[];
+
+    constructor(public service: AppService) { 
+       this.listado_ventas_si_devoluciones = [];
+       this.listado_ventas_no_devoluciones = [];
+       this.listado_top_5_productos = [];
+       this.listado_ventas_plus_anuales = [];
+       this.listado_ventas_normales_anuales = [];
+    }
 
     ngOnInit(){
+        this.get_top_productos();
+        this.get_ventas_devoluciones();
+        this.get_ventas_anuales();
+    }
 
+    /*FUNCIONES*/
+
+    get_top_productos(){
+
+        var response;
+
+        this.service.get_top_5_productos().subscribe(
+
+            data => response = data,
+
+            err => {
+
+                console.log( "Error al consultar el servicio." );
+            },
+
+            () => {
+
+                this.listado_top_5_productos = response;
+                for (const t of this.listado_top_5_productos) {
+                    this.top.push(t.Cantidad);
+                    this.nombre_top.push(t.Producto);
+                }
+            }  
+        );
+    }
+
+    get_ventas_anuales(){
+
+        var response;
+
+        this.service.get_ventas_normales_anuales().subscribe(
+
+            data => response = data,
+
+            err => {
+
+                console.log( "Error al consultar el servicio." );
+            },
+
+            () => {
+
+                this.listado_ventas_normales_anuales = response;
+                for (const t of this.listado_ventas_normales_anuales) {
+                    this.ventas_normales_data.push(t.Total);
+                }
+            }  
+        );
+
+        this.service.get_ventas_plus_anuales().subscribe(
+
+            data => response = data,
+
+            err => {
+
+                console.log( "Error al consultar el servicio." );
+            },
+
+            () => {
+
+                this.listado_ventas_plus_anuales = response;
+                for (const t of this.listado_ventas_plus_anuales) {
+                    this.ventas_label.push(t.Mes);
+                    this.ventas_plus_data.push(t.Total);
+                }
+            }  
+        );
+
+        var set = new Set ()
+    }
+
+    get_ventas_devoluciones(){
+
+        var response;
+
+        this.service.get_grafico_nodevoluciones().subscribe(
+
+            data => response = data,
+
+            err => {
+
+                console.log( "Error al consultar el servicio." );
+            },
+
+            () => {
+                this.listado_ventas_no_devoluciones = response;
+                this.ventas_con_devolucion = Number(this.listado_ventas_no_devoluciones[0]["VentasTotal"]);
+                this.test.push(this.ventas_con_devolucion);
+            }  
+        );
+
+        this.service.get_grafico_sidevoluciones().subscribe(
+
+            data => response = data,
+
+            err => {
+
+                console.log( "Error al consultar el servicio." );
+            },
+
+            () => {
+
+                this.listado_ventas_si_devoluciones = response;
+                this.ventas_sin_devolucion = Number(this.listado_ventas_si_devoluciones[0]["VentasTotal"]);
+                this.test.push(this.ventas_sin_devolucion);
+            }  
+        );
     }
 
     public barChartOptions: ChartOptions = {
@@ -35,23 +160,16 @@ export class GetDashboardComponent implements OnInit
         }
         }
     };
-    public barChartLabels: Label[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+    public barChartLabels: Label[] = this.ventas_label;
     public barChartType: ChartType = 'bar';
     public barChartLegend = true;
     public barChartPlugins = [pluginDataLabels];
 
     public barChartData: ChartDataSets[] = [
-        { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
-        { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' }
+        { data: this.ventas_plus_data, label: 'Ventas Plus' },
+        { data: this.ventas_normales_data, label: 'Ventas Normales' }
     ];
 
-    constructor(public service: AppService) { 
-       this.get_top_productos();
-       this.get_ventas_no_devoluciones();
-       this.get_ventas_si_devoluciones();
-       this.get_ventas_normales_anuales();
-       this.get_ventas_plus_anuales();
-    }
 
     // events
     public chartClicked({ event, active }: { event: MouseEvent, active: {}[] }): void {
@@ -78,7 +196,7 @@ export class GetDashboardComponent implements OnInit
     };
 
     public pieChartLabels: Label[] = [['Ventas sin Devolución'], ['Ventas con Devolución']];
-    public pieChartData: number[] = [this.ventas_sin_devolucion, this.ventas_con_devolucion];
+    public pieChartData: number[] = this.test;
     public pieChartType: ChartType = 'pie';
     public pieChartLegend = true;
     public pieChartPlugins = [pluginDataLabels];
@@ -86,119 +204,20 @@ export class GetDashboardComponent implements OnInit
     {
         backgroundColor: ['rgba(255,0,0,0.3)', 'rgba(0,255,0,0.3)'],
     },];
-
-    public polarAreaChartLabels: Label[] = ['Download Sales', 'In-Store Sales', 'Mail Sales', 'Telesales', 'Corporate Sales'];
-    public polarAreaChartData = [300, 500, 100, 40, 120];
+    
+    public polarAreaChartLabels: Label[] = this.nombre_top;
+    public polarAreaChartData: number[] = this.top;
     public polarAreaLegend = true;
     public polarAreaChartType: ChartType = 'polarArea';
-
-    /*FUNCIONES*/
-
-    get_top_productos(){
-
-        var response;
-
-        this.service.get_top_5_productos().subscribe(
-
-            data => response = data,
-
-            err => {
-
-                console.log( "Error al consultar el servicio." );
-            },
-
-            () => {
-
-                this.listado_top_5_productos = response;
-                console.log(this.listado_top_5_productos);
-            }  
-        );
-    }
-
-    get_ventas_normales_anuales(){
-
-        var response;
-
-        this.service.get_ventas_normales_anuales().subscribe(
-
-            data => response = data,
-
-            err => {
-
-                console.log( "Error al consultar el servicio." );
-            },
-
-            () => {
-
-                this.listado_ventas_normales_anuales = response;
-                console.log(this.listado_ventas_normales_anuales);
-            }  
-        );
-    }
-
-    get_ventas_plus_anuales(){
-
-        var response;
-
-        this.service.get_ventas_plus_anuales().subscribe(
-
-            data => response = data,
-
-            err => {
-
-                console.log( "Error al consultar el servicio." );
-            },
-
-            () => {
-
-                this.listado_ventas_plus_anuales = response;
-                console.log(this.listado_ventas_plus_anuales);
-            }  
-        );
-    }
-
-    get_ventas_no_devoluciones(){
-
-        var response;
-
-        this.service.get_grafico_nodevoluciones().subscribe(
-
-            data => response = data,
-
-            err => {
-
-                console.log( "Error al consultar el servicio." );
-            },
-
-            () => {
-                this.listado_ventas_no_devoluciones = response;
-                this.ventas_con_devolucion = Number(this.listado_ventas_no_devoluciones[0]["VentasTotal"]);
-                this.ventas_con_devolucion = 220;
-                console.log(this.ventas_con_devolucion);
-            }  
-        );
-    }
-
-    get_ventas_si_devoluciones(){
-
-        var response;
-
-        this.service.get_grafico_sidevoluciones().subscribe(
-
-            data => response = data,
-
-            err => {
-
-                console.log( "Error al consultar el servicio." );
-            },
-
-            () => {
-
-                this.listado_ventas_si_devoluciones = response;
-                this.ventas_sin_devolucion = Number(this.listado_ventas_si_devoluciones[0]["VentasTotal"]);
-                this.ventas_sin_devolucion = 20;
-                console.log(this.ventas_sin_devolucion);
-            }  
-        );
-    }
+    public polarAreaChartColors: Array<any> = [
+        {
+          backgroundColor: [
+            'rgba(52, 152, 219,0.4)',
+            'rgba(26, 188, 156,0.4)',
+            'rgba(241, 196, 15,0.4)',
+            'rgba(211, 84, 0,0.4)',
+            'rgba(127, 140, 141,0.4)'
+          ],
+        }
+      ];
 }
